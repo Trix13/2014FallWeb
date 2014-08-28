e0851514aaf2e0f927eca01317eac3974ac33c25
trix13 <fjsmoscatello13@aim.com>
2014-08-28 14:47:38 +0000

    commited for life

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <head>
+            <h1> This is the header<h1>
+        </head>
+        <p> Hello world</p>
+        
+    </body>
+    <footer>
+        This is the footer
+        </footer>
+</html>
